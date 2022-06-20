6cd4b91d929a5573578d2c147f5b607d833c55a6
Kenneth Ossai <kenossai@ymail.com>
2022-06-20 22:18:41 +0100

    added task assign form

--- src/pages/AddTask.js
+++ src/pages/AddTask.js
@@ -3,6 +3,17 @@ import { Link } from 'react-router-dom'
 
 
 class AddTask extends Component{
+
+    state = {
+        name: "",
+        description: ""
+    }
+
+    handleChange = (e) =>{
+        this.setState({
+            [e.target.value]: e.target.value
+        })
+    }
     render(){
         return(
             <div className="container py-5">
@@ -11,15 +22,20 @@ class AddTask extends Component{
                         <div className='card'>
                             <div className="card-header">
                                 <h4>Add Task
-                                    <Link to={'add-task'} className='btn btn-primary btn-sm float-end'>Back</Link>
+                                    <Link to={'/'} className='btn btn-primary btn-sm float-end'>Back</Link>
                                 </h4>
                             </div>
                             <div className="card-body">
                                 <form action="">
                                     <div className="form-group mb-3">
                                         <label htmlFor="">Task Name</label>
-                                        <input type="text" className='form-control' />
+                                        <input type="text" name="task" onChange={this.handleChange} value={this.sate.task} className='form-control' />
                                     </div>
+                                    <div className="form-group mb-3">
+                                        <label htmlFor="">Description</label>
+                                        <textarea className='form-control' onChange={this.handleChange} name="description" value={this.state.description} id="" cols="30" rows="10"></textarea>
+                                    </div>
+                                    <button type='submit' className='btn btn-warning'>Assign Task</button>
                                 </form>
                             </div>
                         </div>
